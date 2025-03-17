f0b6a3a93651ef7d238f3b34c110b0b9dadf6056
jirka8 <jirka.dvorak@gmail.com>
2025-03-17 22:02:28 +0100

    authentification init

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+final class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(
+        #[CurrentUser] ?User $user,
+        AuthenticationUtils $utils
+    ): Response
+    {
+        if ($user) {
+            return $this->redirectToRoute('app_micro_post');
+        }
+
+        $lastUsername = $utils->getLastUsername();
+        $error = $utils->getLastAuthenticationError();
+
+        if ($error) {
+            $this->addFlash('error', $error->getMessage());
+        }
+
+        return $this->render('login/index.html.twig', [
+            'lastUsername' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+    }
+}
